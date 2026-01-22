8774c1c84b22152609a5c07a111e935a58d208f1
redindelible <redindelible@gmail.com>
2026-01-21 23:26:17 -0800

    refactored lowering

--- flywheel-lower/src/lib.rs
+++ flywheel-lower/src/lib.rs
@@ -7,104 +7,192 @@ use by_address::ByAddress;
 use flywheel_ast as ast;
 use flywheel_error::{CompileMessage, CompileResult};
 use flywheel_sources::Symbol;
-
-use crate::namespace::{Builtin, Item, NamespaceId, NamespaceMap};
+use crate::context::{LoweringContext, WithCollectedNames};
+use crate::namespace::{Builtin, Item, Namespace};
 
 enum Type<'ast> {
     U32,
     Struct(&'ast ast::Struct<'ast>),
 }
 
-fn resolve_name_as_type<'ast>(
-    id: NamespaceId,
-    namespaces: &NamespaceMap<'ast>,
-    file_namespaces: &HashMap<&[Symbol], NamespaceId>,
-    name: Symbol,
-) -> CompileResult<Type<'ast>> {
-    match namespaces.resolve(id, name) {
-        None => {
-            let message = format!("No type found called {}", namespaces.resolve_symbol(name));
-            Err(CompileMessage::error(message))
+mod context {
+    use std::collections::HashMap;
+    use std::marker::PhantomData;
+    use std::ops::Deref;
+    use std::sync::Arc;
+    use flywheel_sources::{SourceMap, Symbol};
+    use flywheel_ast as ast;
+    use crate::{CollectedNames, StructFields, Type};
+
+    #[repr(C)]
+    pub struct LoweringContext<'ast, S> {
+        _state: PhantomData<S>,
+
+        sources: Arc<SourceMap>,
+        ast: &'ast ast::Module,
+        builtins: &'ast HashMap<&'static str, Symbol>,
+
+        collected_names: Option<CollectedNames<'ast>>,
+        struct_fields: Option<StructFields<'ast>>,
+    }
+
+    impl<'ast> LoweringContext<'ast, ()> {
+        pub fn new(ast: &'ast ast::Module, builtins: &'ast HashMap<&'static str, Symbol>) -> LoweringContext<'ast, ()> {
+            LoweringContext {
+                _state: PhantomData,
+
+                sources: Arc::clone(&ast.sources),
+                ast,
+                builtins,
+
+                collected_names: None,
+                struct_fields: None,
+            }
         }
-        Some(Item::Function(_)) => {
-            let message = format!("No type found called {}", namespaces.resolve_symbol(name));
-            Err(CompileMessage::error(message))
+
+        pub fn ast(&self) -> &'ast ast::Module {
+            self.ast
         }
-        Some(Item::Imported(import)) => {
-            assert!(import.anchor.is_none());
-            resolve_name_as_type(file_namespaces[import.path], namespaces, file_namespaces, name) // todo cache this and prevent cycles
+
+        pub fn builtins(&self) -> &'ast HashMap<&'static str, Symbol> {
+            self.builtins
         }
-        Some(Item::Struct(struct_)) => Ok(Type::Struct(struct_)),
-        Some(Item::Builtin(Builtin::U32)) => Ok(Type::U32),
+
+        pub fn get_symbol(&self, symbol: Symbol) -> &str {
+            self.sources.get_symbol(symbol)
+        }
+
+        pub fn with_collected_names(mut self, collected_names: CollectedNames<'ast>) -> LoweringContext<'ast, WithCollectedNames> {
+            self.collected_names = Some(collected_names);
+            unsafe { std::mem::transmute(self) }
+        }
+    }
+
+    pub struct WithCollectedNames;
+
+    impl<'ast> Deref for LoweringContext<'ast, WithCollectedNames> {
+        type Target = LoweringContext<'ast, ()>;
+
+        fn deref(&self) -> &Self::Target {
+            unsafe { std::mem::transmute::<&Self, &Self::Target>(self) }
+        }
+    }
+
+    impl<'ast> LoweringContext<'ast, WithCollectedNames> {
+        pub fn collected_names(&self) -> &CollectedNames<'ast> {
+            unsafe { self.collected_names.as_ref().unwrap_unchecked() }
+        }
+
+        pub fn with_struct_fields(mut self, struct_fields: StructFields<'ast>) -> LoweringContext<'ast, WithStructFields> {
+            self.struct_fields = Some(struct_fields);
+            unsafe { std::mem::transmute(self) }
+        }
+    }
+
+    pub struct WithStructFields;
+
+    impl<'ast> LoweringContext<'ast, WithStructFields> {
+        pub fn struct_fields(&self) -> &StructFields<'ast> {
+            unsafe { self.struct_fields.as_ref().unwrap_unchecked() }
+        }
     }
 }
 
-fn resolve_type<'ast>(
-    id: NamespaceId,
-    namespaces: &NamespaceMap<'ast>,
-    file_namespaces: &HashMap<&[Symbol], NamespaceId>,
-    ast_ty: &ast::Type<'ast>,
-) -> CompileResult<Type<'ast>> {
-    resolve_name_as_type(id, namespaces, file_namespaces, ast_ty.name)
+pub struct CollectedNames<'ast> {
+    pub file_namespaces: HashMap<&'ast [Symbol], Arc<Namespace<'ast>>>,
+    pub prelude_ns: Arc<Namespace<'ast>>,
+}
+
+pub struct StructFields<'ast> {
+    struct_fields: HashMap<ByAddress<&'ast ast::Struct<'ast>>, HashMap<Symbol, Type<'ast>>>
+}
+
+impl<'ast> LoweringContext<'ast, WithCollectedNames> {
+    fn resolve_name_as_type(&self, in_ns: &Namespace<'ast>, name: Symbol) -> CompileResult<Type<'ast>> {
+        match in_ns.resolve(name) {
+            None => {
+                let message = format!("No type found called {}", self.get_symbol(name));
+                Err(CompileMessage::error(message))
+            }
+            Some(Item::Function(_)) => {
+                let message = format!("No type found called {}", self.get_symbol(name));
+                Err(CompileMessage::error(message))
+            }
+            Some(Item::Imported(import)) => {
+                assert!(import.anchor.is_none());
+                self.resolve_name_as_type(&self.collected_names().file_namespaces[import.path], name) // todo cache this and prevent cycles
+            }
+            Some(Item::Struct(struct_)) => Ok(Type::Struct(struct_)),
+            Some(Item::Builtin(Builtin::U32)) => Ok(Type::U32),
+        }
+    }
+
+    fn resolve_type(
+        &self,
+        in_ns: &Namespace<'ast>,
+        ast_ty: &ast::Type<'ast>,
+    ) -> CompileResult<Type<'ast>> {
+        self.resolve_name_as_type(in_ns, ast_ty.name)
+    }
 }
 
 pub const BUILTINS: &[&str] = &["u32"];
 
 pub fn lower(ast: &ast::Module, builtins: &HashMap<&'static str, Symbol>) -> CompileResult<()> {
-    let mut namespaces = NamespaceMap::new(Arc::clone(&ast.sources));
-    let prelude_ns = namespaces.add_namespace(None);
-    namespaces.add(prelude_ns, builtins["u32"], Item::Builtin(Builtin::U32)).unwrap();
+    let ctx = LoweringContext::new(ast, builtins);
 
-    let mut file_namespaces: HashMap<&[Symbol], NamespaceId> = HashMap::new();
+    let mut prelude_ns = Namespace::new_root(Arc::clone(&ast.sources));
+    prelude_ns.add(builtins["u32"], Item::Builtin(Builtin::U32)).unwrap();
+    let prelude_ns = Arc::new(prelude_ns);
+
+    let mut file_namespaces: HashMap<&[Symbol], Arc<Namespace>> = HashMap::new();
     for (path_in_module, file) in &ast.contents {
-        let file_ns = namespaces.add_namespace(Some(prelude_ns));
+        let mut file_ns = Namespace::new_child(Arc::clone(&prelude_ns));
 
         for top_level in file.top_levels() {
             match *top_level {
                 ast::TopLevel::Import(import) => {
-                    namespaces.add(file_ns, import.item, Item::Imported(import))?;
+                    file_ns.add(import.item, Item::Imported(import))?;
                 }
                 ast::TopLevel::Function(func) => {
-                    namespaces.add(file_ns, func.name, Item::Function(func))?;
+                    file_ns.add(func.name, Item::Function(func))?;
                 }
                 ast::TopLevel::Struct(struct_) => {
-                    namespaces.add(file_ns, struct_.name, Item::Struct(struct_))?;
+                    file_ns.add(struct_.name, Item::Struct(struct_))?;
                 }
             }
         }
-        file_namespaces.insert(path_in_module, file_ns);
+        file_namespaces.insert(path_in_module, Arc::new(file_ns));
     }
 
-    type StructFields<'ast> = HashMap<Symbol, Type<'ast>>;
-    let mut struct_fields: HashMap<ByAddress<&ast::Struct>, StructFields> = HashMap::new();
+    let ctx = ctx.with_collected_names(CollectedNames {
+        file_namespaces,
+        prelude_ns,
+    });
 
-    struct FunctionSignature<'ast> {
-        return_type: Type<'ast>,
-    }
-    let mut function_signatures: HashMap<ByAddress<&ast::Function>, FunctionSignature> = HashMap::new();
-
+    let mut struct_fields = HashMap::new();
     for (path_in_module, file) in &ast.contents {
-        let file_namespace = file_namespaces[path_in_module.as_slice()];
+        let file_namespace = &*ctx.collected_names().file_namespaces[path_in_module.as_slice()];
         for top_level in file.top_levels() {
             match *top_level {
                 ast::TopLevel::Struct(struct_) => {
-                    let mut fields: StructFields = HashMap::new();
+                    let mut fields = HashMap::new();
 
                     for field in struct_.fields {
-                        let ty = resolve_type(file_namespace, &namespaces, &file_namespaces, &field.ty)?;
+                        let ty = ctx.resolve_type(file_namespace, &field.ty)?;
                         fields.insert(field.name, ty);
                     }
                     assert!(struct_fields.insert(ByAddress(struct_), fields).is_none());
                 }
-                ast::TopLevel::Function(function) => {
-                    let return_type = resolve_type(file_namespace, &namespaces, &file_namespaces, &function.return_type)?;
-                    let signature = FunctionSignature { return_type };
-                    assert!(function_signatures.insert(ByAddress(function), signature).is_none());
-                }
+                ast::TopLevel::Function(_) => (),
                 ast::TopLevel::Import(_) => (),
             };
         }
     }
+
+    let ctx = ctx.with_struct_fields(StructFields {
+        struct_fields
+    });
 
     Ok(())
 }
--- flywheel-lower/src/namespace.rs
+++ flywheel-lower/src/namespace.rs
@@ -2,7 +2,6 @@ use std::collections::HashMap;
 use std::sync::Arc;
 
 use flywheel_ast as ast;
-use flywheel_common::{declare_key_type, KeyData, ReservableMap};
 use flywheel_error::{CompileMessage, CompileResult};
 use flywheel_sources::{SourceMap, Span, Symbol};
 
@@ -28,47 +27,39 @@ impl<'ast> Item<'ast> {
     }
 }
 
-declare_key_type! {
-    pub struct NamespaceId;
-}
-
-pub struct NamespaceMap<'ast> {
+pub struct Namespace<'ast> {
     sources: Arc<SourceMap>,
-    map: ReservableMap<NamespaceId, Namespace<'ast>>
-}
-
-struct Namespace<'ast> {
-    sources: Arc<SourceMap>,
-    parent: Option<NamespaceId>,
+    parent: Option<Arc<Namespace<'ast>>>,
     items: HashMap<Symbol, Item<'ast>>,
 }
 
-impl<'ast> NamespaceMap<'ast> {
-    pub fn new(sources: Arc<SourceMap>) -> NamespaceMap<'ast> {
-        NamespaceMap {
+impl<'ast> Namespace<'ast> {
+    pub fn new_root(sources: Arc<SourceMap>) -> Namespace<'ast> {
+        Namespace {
             sources,
-            map: ReservableMap::new(),
+            parent: None,
+            items: HashMap::new(),
         }
     }
 
-    pub fn add_namespace(&mut self, parent: Option<NamespaceId>) -> NamespaceId {
-        self.map.add(Namespace {
-            sources: Arc::clone(&self.sources),
-            parent,
+    pub fn new_child(parent: Arc<Namespace<'ast>>) -> Namespace<'ast> {
+        Namespace {
+            sources: Arc::clone(&parent.sources),
+            parent: Some(parent),
             items: HashMap::new(),
-        })
+        }
     }
 
-
-    pub fn add(&mut self, id: NamespaceId, name: Symbol, item: Item<'ast>) -> CompileResult<()> {
+    pub fn add(&mut self, name: Symbol, item: Item<'ast>) -> CompileResult<()> {
         use std::collections::hash_map::Entry;
 
-        match self.map[id].items.entry(name) {
+        match self.items.entry(name) {
             Entry::Vacant(vacant) => {
                 vacant.insert(item);
             }
             Entry::Occupied(occupied) => {
                 let name = self.sources.get_symbol(name);
+                // todo can we just have error take a callback that expects a source instead
                 let message = CompileMessage::error(format!("There's already a thing called {}", name))
                     .with_span(item.span().unwrap())
                     .with_child(
@@ -80,21 +71,21 @@ impl<'ast> NamespaceMap<'ast> {
         Ok(())
     }
 
-    pub fn resolve(&self, id: NamespaceId, name: Symbol) -> Option<&Item<'ast>> {
-        let mut search_in: &Namespace = &self.map[id];
+    pub fn resolve(&self, name: Symbol) -> Option<&Item<'ast>> {
+        let mut search_in: &Namespace = self;
         loop {
             // dbg!(search_in.items.keys().map(|&sym| self.resolve_symbol(sym)).collect::<Vec<_>>());
             if let Some(item) = search_in.items.get(&name) {
                 return Some(item);
-            } else if let Some(parent) = search_in.parent {
-                search_in = &self.map[parent];
+            } else if let Some(parent) = &search_in.parent {
+                search_in = parent;
             } else {
                 return None;
             }
         }
     }
 
-    pub fn resolve_symbol(&self, name: Symbol) -> &str {
+    pub fn get_symbol(&self, name: Symbol) -> &str {
         self.sources.get_symbol(name)
     }
 }
